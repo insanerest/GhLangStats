f166883ee61b4af1a3df5fd0015cb3643797f8d7
Insanerest <157520929+insanerest@users.noreply.github.com>
2025-07-23 18:55:05 -0400

    Updated a fetcher

--- src/fetchers/repoFetch.js
+++ src/fetchers/repoFetch.js
@@ -3,7 +3,7 @@ const path = require("path");
 const https = require("https");
 require("dotenv").config({ quiet: true, debug: false });
 
-const CACHE_DIR = path.join(__dirname, "..", "cache");
+const CACHE_DIR = path.resolve("src", "cache");
 if (!fs.existsSync(CACHE_DIR)) fs.mkdirSync(CACHE_DIR);
 
 const GITHUB_API = "https://api.github.com";
